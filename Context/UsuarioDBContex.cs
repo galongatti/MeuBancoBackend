4e2f80d7fef77274925bbb255c4d61cffe283e11
galongatti <galongatti@hotmail.com>
2023-11-02 11:24:17 -0300

    Adicionar arquivos de projeto.

---
+++ Context/UsuarioDBContex.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuBancoBackend.Context
+{
+    public class UsuarioDBContex : IdentityDbContext<IdentityUser>
+    {
+        public UsuarioDBContex(DbContextOptions<UsuarioDBContex> options) : base(options) { }
+    }
+}
