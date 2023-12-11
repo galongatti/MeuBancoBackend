200d8b1b2c384a652867f029c74e8547e60b4c62
galongatti <galongatti@hotmail.com>
2023-12-11 11:15:14 -0300

    novas migrations

--- Context/Configuration/EmprestimosConfiguration.cs
+++ Context/Configuration/EmprestimosConfiguration.cs
@@ -14,6 +14,8 @@ namespace MeuBancoBackend.Context.Configuration
             builder.HasKey(e => e.IdEmprestimo);
             builder.Property(e => e.IdEmprestimo).HasColumnType("BIGINT").ValueGeneratedOnAdd();
             builder.Property(e => e.ValorSolicitado).HasColumnType("DECIMAL(18,2)");
+            builder.Property(e => e.PercentualSalario).HasColumnType("DECIMAL(18,2)");
+            builder.Property(e => e.NotaSalario).HasColumnType("INT");
             builder.Property(e => e.IdPessoa).HasColumnType("BIGINT");
             builder.HasOne(e => e.Pessoa).WithMany(e => e.Emprestimos)
                 .HasForeignKey(x => x.IdPessoa).IsRequired(true).HasConstraintName("FK_EMPRESTIMO_PESSOA");
---
+++ Migrations/MeuBancoDB/20231120161228_NotaAnaliseCadastro.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeuBancoBackend.Migrations.MeuBancoDB
+{
+    public partial class NotaAnaliseCadastro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NotalSalario",
+                table: "Emprestimo",
+                type: "INT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PercentualSalario",
+                table: "Emprestimo",
+                type: "DECIMAL(18,2)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotalSalario",
+                table: "Emprestimo");
+
+            migrationBuilder.DropColumn(
+                name: "PercentualSalario",
+                table: "Emprestimo");
+        }
+    }
+}
